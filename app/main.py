a0051a621ea6a5644da745811b84135b6bce28d0
mdrasiksha <mdrasiksha1992@gmail.com>
2026-03-31 12:03:37 +0530

    update api1

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .reminder import start_scheduler
 from . import models
 from .database import Base, engine
 from .routes import leads, properties, webhook
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -20,3 +21,12 @@ app.include_router(webhook.router, prefix="/webhook", tags=["Webhook"])
 @app.get("/")
 def home():
     return {"message": "Real Estate CRM running 🚀"}
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
